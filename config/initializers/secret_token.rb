2c1d5e33287c73bdcf2a69599468a32623e7c223
Łukasz Bandzarewicz <lucassus@gmail.com>
2011-06-27 17:39:42 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KrugForumDemo::Application.config.secret_token = '84fcc12c3e2ad852e1e1e0ffbfb1c152f8beb0b7c53bad9bed1f5ccf22057b949816cce2e7ff59a961187f3b2d7d1c044a2ed84f52605af5b476984469004467'
